9d0ce8dcd5d067c8c3ca80d26745621266020da5
Deekshitha145 <deekshitha_145@DESKTOP-VQG8G6F.localdomain>
2025-09-19 09:41:32 +0000

    Add Jenkinsfile and login-app project setup

--- src/main/java/com/example/servlets/LoginServlet.java
+++ src/main/java/com/example/servlets/LoginServlet.java
@@ -1,7 +1,7 @@
 package com.example.servlets;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
